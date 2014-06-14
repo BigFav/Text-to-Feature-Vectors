fe3f85c52ca0a033b04cc587983db326c4ecfcd3
Favian Contreras <fnc4@cornell.edu>
2014-06-14 04:06:08 -0400

    Modularized the feature vector portion.

--- text-to-svm.py
+++ text-to-svm.py
@@ -26,6 +26,27 @@ def parse_and_tokenize(line):
     line.insert(0, line.pop())
     return line
 
+
+def get_feature_vectors(total_words, data_words, data_labels=None):
+    ft_vecs = []
+    for i, word_dict in enumerate(data_words):
+        if not data_labels or (data_labels[i] == '0'):
+            ft_vecs.append('-1')
+        else:
+            ft_vecs.append(data_labels[i])
+
+        ft_ids = []
+        for word, count in word_dict.iteritems():
+            ft_id = total_words[word]
+            ft_ids.append((ft_id, (' %s:%s' % (ft_id, count))))
+        ft_ids = sorted(ft_ids, key=itemgetter(0))  # Must be in order
+
+        ft_vecs.extend(tup[1] for tup in ft_ids)
+        ft_vecs.append('\n')
+
+    return ''.join(ft_vecs)
+
+
 def main():
     train_words = []
     train_labels = []
@@ -46,7 +67,7 @@ def main():
 
     # The order the word is seen in the dicts will be the ft id
     total_words = {}
-    i = 0
+    i = 1
     for lst in train_words:
         for j, word in enumerate(lst.iterkeys()):
             if word not in total_words:
@@ -60,40 +81,12 @@ def main():
         i += j + 1
 
     # Create the ft vecs and files
-    ft_vecs = []
-    for i, word_dict in enumerate(train_words):
-        if train_labels[i] == '0':
-            train_labels[i] = '-1'
-        ft_vecs.append(train_labels[i])
-
-        ft_ids = []
-        for word, count in word_dict.iteritems():
-            ft_id = total_words[word] + 1
-            ft_ids.append((ft_id, (' %s:%s' % (ft_id, count))))
-        ft_ids = sorted(ft_ids, key=itemgetter(0))  # Must be in order
-
-        ft_vecs.extend(tup[1] for tup in ft_ids)
-        ft_vecs.append('\n')
-
-    ft_vecs = ''.join(ft_vecs)
-    with open('svm_train.train', 'w') as train:
+    ft_vecs = get_feature_vectors(total_words, train_words, train_labels)
+    with open('svm_train1.train', 'w') as train:
         train.write(ft_vecs)
 
-    ft_vecs = []
-    for word_dict in test_words:
-        ft_vecs.append('-1')
-
-        ft_ids = []
-        for word, count in word_dict.iteritems():
-            ft_id = total_words[word] + 1
-            ft_ids.append((ft_id, (' %s:%s' % (ft_id, count))))
-        ft_ids = sorted(ft_ids, key=itemgetter(0))
-
-        ft_vecs.extend(tup[1] for tup in ft_ids)
-        ft_vecs.append('\n')
-
-    ft_vecs = ''.join(ft_vecs)
-    with open('svm_test.test', 'w') as test:
+    ft_vecs = get_feature_vectors(total_words, test_words)
+    with open('svm_test1.test', 'w') as test:
        test.write(ft_vecs)
  
 if __name__ == '__main__':
